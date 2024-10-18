4fbad82224153ac890dca207c7dc94f72a20ce7f
Tomcat13 <reedyt22@gmail.com>
2024-10-18 10:41:17 -0400

    I think I'm understanding this now

--- corpus_counter.py
+++ corpus_counter.py
@@ -1,3 +1,7 @@
-def token_count(corpus, token):
-    # makes token case-insensitive
+def token_count(token):
+    # reads corpus.txt
+    with open('corpus.txt', 'r') as file:
+        corpus = file.read()
+
+    # makes token case-insensitive, returns count
     return corpus.lower().split().count(token.lower())
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from dummy.corpus_counter import token_count
 
-def report_count(corpus, token):
-    count = token_count(corpus, token)
+def report_count(token):
+    count = token_count(token)
     print(f'The term {token} shows up in the corpus {count} times.')
