1613a7702b00edd2f4cca27e778cf2eb53d8d2ec
Tomcat13 <reedyt22@gmail.com>
2024-10-18 11:08:29 -0400

    i think it works!

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 
 def report_count(token):
-    test = "subscriber"
+    #test = "subscriber"
     from dummy.corpus_counter import token_count
-    
-    count = token_count(test, test)
+
+    count = token_count(token)
     #print(f'The term {token} shows up in the corpus {count} times.')
     print(count)
