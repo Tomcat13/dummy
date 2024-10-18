e2e9180efea0fc2b4839de90ac2d0bf0f70730fb
Tomcat13 <reedyt22@gmail.com>
2024-10-18 10:38:14 -0400

    change function to report_count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from dummy.corpus_counter import token_count
 
-def print_count(corpus, token):
+def report_count(corpus, token):
     count = token_count(corpus, token)
     print(f'The term {token} shows up in the corpus {count} times.')
