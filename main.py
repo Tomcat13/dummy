644af911326db3b184866b1ece84ef343b86ac30
Tomcat13 <reedyt22@gmail.com>
2024-10-18 10:51:51 -0400

    once agian, idk why this is happening

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from dummy.corpus_counter import token_count
 
 def report_count(token):
-    
-    count = token_count()
+    # reads corpus.txt
+    with open('corpus.txt', 'r') as file:
+        corpus = file.read()
+    token = "subscriber"
+    count = token_count(corpus, token)
     print(f'The term {token} shows up in the corpus {count} times.')
